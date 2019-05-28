1f9a61c95cd5c826a3510af20bbb1ad91c874ba3
boomgoat <talhaiqbal11@gmail.com>
2019-05-28 13:02:17 -0700

    playing around with colors and gifs.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from '../logo.svg';
+import logo from '../Resources/Logo/logo.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Gullwing
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/boomgoat/Gullwing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out the repo
         </a>
       </header>
     </div>
