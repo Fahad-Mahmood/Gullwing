4db724274daa6136ad76ae6ca4cc37a92a6ad528
boomgoat <talhaiqbal11@gmail.com>
2019-06-01 13:23:48 -0700

    basic landing complete. Routing added. Waiting on additional components.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import logo from '../../Resources/Logo/logo.gif';
+// import logo from '../../Resources/Logo/logo.gif';
 import './header.css';
-import { Col, Row } from 'reactstrap';;
+import { Col, Row, Container } from 'reactstrap';;
 
 function Header() {
   return (
-    <Row className="App-header" >
-      <Col md={2} className="logo" >
+    <Container fluid="true" className="App-header" >
+      <Row>
+        <Col md={2} className="logo" >
 
-      </Col>
-      <Col md={{size: 2, offset:1}} className="Header-block">
-        
-      </Col>
-      <Col md={6} className="banner">
-      </Col>
-      <Col md={{size: 4, offset:1 }} className="Intro-text">
-        <h1>
-          All the resources<br/>
-          you'll ever need.
-        </h1>
-        <p>scroll down & select course</p>
-      </Col>
-    </Row>
+        </Col>
+        <Col md={{size: 1, offset:2}} className="Header-block">
+          
+        </Col>
+        <Col md={6} className="banner">
+        </Col>
+        <Col md={{size: 4, offset:1 }} className="Intro-text">
+          <h1>
+            All the resources<br/>
+            you'll ever need.
+          </h1>
+          <p>scroll down & select course</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={1} className="Header-block-small" >
+
+        </Col>
+        <Col md={{size:5, offset:4}} className="Intro-Block text-left" >
+          <br/>
+          <h5>Give your career a boost!</h5>
+          <br/>
+          <p>Explore a wide variety of courses specifically designed<br/>
+          to boost your career. Beat the competition with a superior<br/>
+          skillset.
+          <br/>
+          Join the revolution!
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Header from '../Header/header';
 import './landing.css';
-import { Container, Col, Row } from 'reactstrap';
+import { Col } from 'reactstrap';
 
 function Landing(){
     return(
-        <Container fluid="true">
-            <Col md={11}>
-                <Header/>
-            </Col>
-        </Container>
+        <Col md={11} className="Main">
+            <Header/>
+        </Col>
     );
 }
 
